40d7d619fd69513fb836e4cc97cf81b02bd53578
BIG_DEV360 <shittuabdquadri0@gmail.com>
2024-12-02 09:30:15 -0800

    Worked on vieW-account.js

--- WEB_ROOT/JS/SHARED/VIEW-ACCOUNT/view-account.js
+++ WEB_ROOT/JS/SHARED/VIEW-ACCOUNT/view-account.js
@@ -8,3 +8,21 @@ function menuClick() {
     sidebar.classList.add('hidden');
   }
 }
+import { LoginAdvertiser, RegisterAdvertiser, GetAll } from "/WEB_ROOT/UTILITIES/FUNCTION_UTILIITIES/API/apiHelper.js";
+
+export async function RegisterAdvertiser (advertiserFirstName, advertiserLastName,advertiserUserName,advertiserPhoneNumber,advertiserEmailAddress,advertiserStatus,advertiserPassword,advertiserConfirmPassword)  {
+  try {
+      const result = await RegisterAdvertiser  (advertiserFirstName, advertiserLastName,advertiserUserName,advertiserPhoneNumber,advertiserEmailAddress,advertiserStatus,advertiserPassword,advertiserConfirmPassword) ;
+      console.log(result);
+
+    const userContainer = document.getElementById("userInfo_Div")
+    userContainer.innerHTML  `
+    <h2>User Info</h2>
+    <p><strong>Name:</strong> ${advertiserFirstName}</p>
+    <p><strong>Email:</strong> ${advertiserEmailAddress}</p>
+    <p><strong>Phone:</strong> ${advertiserUserName}</p>
+    <p><strong>Address:</strong> ${advertiserPhoneNumber}</p>`
+  } catch (error) {
+      console.error("Error creating advertiser:", error);
+  }
+}
